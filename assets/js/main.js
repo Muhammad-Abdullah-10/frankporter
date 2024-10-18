8d00108f46a565686417ed12249b6f75679aa5d6
Muhammad-Abdullah-10 <59797973+Muhammad-Abdullah-10@users.noreply.github.com>
2024-10-18 15:28:24 +0500

    Faq js fixed and about us added

--- assets/js/main.js
+++ assets/js/main.js
@@ -74,7 +74,25 @@ $(document).ready(function(){
       });
     
       $(".faq-area-wrapper .question").click(function(){
-        $(this).closest(".faq-area-wrapper").find(".answer").toggleClass("hidden");
-        $(".answer").not($(this).closest(".faq-area-wrapper").find(".answer")).addClass("hidden");
+        // $(this).closest(".faq-area-wrapper").find(".answer").toggleClass("hidden");
+        // $(".").not($(this).closest(".faq-area-wrapper").find(".answer")).addClass("hidden");
+        $(this).closest(".question").find(".answer").slideToggle();
+        if(!$(this).closest(".question").find(".answer").hasClass("hidden")){
+            $(this).$(".faq-icon").html(`
+              <svg class="w-3 h-3 fill-current" viewBox="0 -192 469.33333 469" xmlns="http://www.w3.org/2000/svg"><path d="m437.332031.167969h-405.332031c-17.664062 0-32 14.335937-32 32v21.332031c0 17.664062 14.335938 32 32 32h405.332031c17.664063 0 32-14.335938 32-32v-21.332031c0-17.664063-14.335937-32-32-32zm0 0"/></svg>
+              `);
+        }
+        else{
+          $(".faq-icon").html(`
+             <svg class="toggle-icon w-3 h-3 fill-current" viewBox="0 -192 469.33333 469" xmlns="http://www.w3.org/2000/svg"><path d="m437.332031.167969h-405.332031c-17.664062 0-32 14.335937-32 32v21.332031c0 17.664062 14.335938 32 32 32h405.332031c17.664063 0 32-14.335938 32-32v-21.332031c0-17.664063-14.335937-32-32-32zm0 0"/></svg>
+            `);
+        }
       })
+      $(".faq-area-wrapper .question").click(function() {
+        // Slide up all answers except the one that's clicked
+        // $(".faq-area-wrapper .answer").not($(this).next()).slideUp();
+        
+        // Toggle the clicked FAQ answer
+        // $(this).next(".answer").slideToggle();
+      });
   });
