8eafa58d27603ea762c8f45144be562fdf9c8f50
Muhammad-Abdullah-10 <59797973+Muhammad-Abdullah-10@users.noreply.github.com>
2024-11-08 21:39:55 +0500

    video tab added into gallery

--- assets/js/main.js
+++ assets/js/main.js
@@ -198,5 +198,15 @@ $(document).ready(function(){
           $(targetGrid).fadeIn();
         });
       });
+
+      var video = $('.wrap-video').hover(hoverVideo, hideVideo);
+
+      function hoverVideo(e) {  
+          $('video', this).get(0).play(); 
+      }
+  
+      function hideVideo(e) {
+          $('video', this).get(0).pause(); 
+      }
       
   });
